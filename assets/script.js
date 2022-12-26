d5eb037ff5e90da27153f8d1f8781cc6303ee64f
Maya Lynn <mayalynncohen@gmail.com>
2022-12-26 13:47:09 -0800

    changed the name

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// 1. make timer 
+// - have it show up on the page
+// - make it stop when all questions are answered
+// - subtract time if question answered wrong
+// - make stop when it hits 0
+// 2. make question objects 
+// - add question
+// - add answers (1 right 3 wrong)
+// 3. connect obects to buttons
+// - let user know if answer is correct
+// - let user know when wrong + substract time + correct answer shown
+// - make the answers be in random order
+// 4. scores are kept
+// - open a form for user to write name
+// - time remaining becomes score
+// - user is brought to score board when name is entered
+// - button that leads to score board to left corner
